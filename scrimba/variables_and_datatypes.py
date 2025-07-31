0a5809f5e04d0738738416cb08f524663ff0724c
Atakan Onay <ata.onay4@gmail.com>
2025-07-31 23:07:07 +0300

    added second assigment

---
+++ scrimba/variables_and_datatypes.py
@@ -0,0 +1,7 @@
+Item_name = "Hammer"
+price = 35.4
+in_stock = 4
+space = "-"
+
+print(str(Item_name) + space + space + str(price) + space + str(in_stock))
+print('Variables & Datatypes - Exercise')
