42f604b1b8514532ffc20014cd8ff7461ee82f53
Atakan Onay <ata.onay4@gmail.com>
2025-08-08 16:31:48 +0300

    created templates and html file to return html file with render(request, ) func.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def say_hello(resuest):
-    return HttpResponse('Hello world')
+def say_hello(request):
+    return render(request, 'hello.html')
