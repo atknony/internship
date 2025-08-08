2708ac071a6bea62a3c290a321e1c81543cfc699
Atakan Onay <ata.onay4@gmail.com>
2025-08-08 17:43:41 +0300

    created todos, added settings.py

--- TodoApp/todoapi/settings.py
+++ TodoApp/todoapi/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'todos',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'todoapi.urls'
 
 TEMPLATES = [
