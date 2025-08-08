ee6606eab01c85b97b588f4baa1ba64a0655ae5d
Atakan Onay <ata.onay4@gmail.com>
2025-08-08 18:02:18 +0300

    added Todo serializer and viewset

---
+++ TodoApp/todos/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = [
+            "id", "title", "description", "is_completed",
+            "due_date", "created_at", "updated_at"
+        ]
+        ready_only_fields = ["id", "created_at", "updated_at"]
+
